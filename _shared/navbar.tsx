950fcec7fcb11ec5b3a8896f95c4030c7346805f
Clément DOUIN <clement.douin@posteo.net>
2022-02-10 13:48:10 +0100

    force page reload on change lang

--- _shared/navbar.tsx
+++ _shared/navbar.tsx
@@ -1,10 +1,10 @@
-import React, {FC} from "react";
+import React, {FC, useState} from "react";
 import {useRouter} from "next/router";
 
 import Link from "./link";
 import Img from "./img";
 import avatar from "./avatar.jpeg";
-import {langs, LangProps, useI18n} from "./i18n";
+import {langs, LangProps, useI18n, parseLang} from "./i18n";
 import cs from "./navbar.module.scss";
 
 export type NavbarProps = LangProps;
@@ -12,10 +12,12 @@ export type NavbarProps = LangProps;
 const Navbar: FC<NavbarProps> = props => {
   const {t} = useI18n(props.lang, ["experience", "project"]);
   const router = useRouter();
+  const [lang, setLang] = useState(props.lang);
 
   function changeLang(evt: React.ChangeEvent<HTMLSelectElement>) {
+    setLang(parseLang(evt.target.value));
     const subpath = router.asPath ? router.asPath.slice(3) : "";
-    router.push(`/${evt.target.value}/${subpath}`);
+    window.location.pathname = `/${evt.target.value}${subpath}`;
   }
 
   return (
@@ -28,7 +30,7 @@ const Navbar: FC<NavbarProps> = props => {
           <span className={cs.avatarTitle}>Clément DOUIN</span>
         </Link>
       </div>
-      <select className={cs.lang} onChange={changeLang} value={props.lang}>
+      <select className={cs.lang} onChange={changeLang} value={lang}>
         {langs.map(lang => (
           <option key={lang} value={lang}>
             {lang}
